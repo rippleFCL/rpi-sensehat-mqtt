adb284455cdf5f8685f19d06f7fed23f2657e4ee
cgomesu <cf365@cornell.edu>
2022-12-27 11:10:56 -0300

    Extend config and update sensehat implementation
    Add sensehat user configurable variables to config.py logic.
    
    Streamline the config.py implementation.
    
    Update sensehat.py implementation.

--- sensehat/sensehat.py
+++ sensehat/sensehat.py
@@ -15,14 +15,7 @@ class SenseHatSensor():
     """
     Generates a SenseHAT sensor object
     """
-    # class defaults
-    # number of decimals for float variables
-    ROUNDING = 4
-    # converts Gs (default) to meters/square second; set to 1 for default
-    ACCELERATION_MULTIPLYER = 9.80665
-    # convert the rotational intensity in radians/second (default) to something else; set to 1 for default
-    GYROSCOPE_MULTIPLYER = 1
-    # data keys
+    # data keys label convention for the class objects
     TIME = 'time'
     PRESSURE = 'pressure'
     TEMPERATURE = 'temperature'
@@ -40,15 +33,22 @@ class SenseHatSensor():
     ACCELERATION_02 = 'y'
     ACCELERATION_03 = 'z'
 
+    # TODO: add new config variables to attr
     def __init__(self,
                 zone:str,
                 room:str,
                 sensor:str,
-                low_light:bool = True):
+                low_light:bool = True,
+                rounding:int = 4,
+                acceleration_multiplier:float = 1.0,
+                gyroscope_multiplier:float = 1.0):
         self.zone = zone
         self.room = room
         self.sensor = sensor
         self.low_light = low_light
+        self.rounding = rounding
+        self.acceleration_multiplier = acceleration_multiplier
+        self.gyroscope_multiplier = gyroscope_multiplier
         # sensors variables
         self.__time = None
         self.__pressure = None
@@ -59,7 +59,7 @@ class SenseHatSensor():
         self.__acceleration_01 = self.__acceleration_02 = self.__acceleration_03 = None
         # create a private SenseHat object to interact with the sensors API
         self.__sense = SenseHat()
-        # read initial sensor valus
+        # read initial sensor values
         self.data = self.sensors_data()
         # TODO: log sensehat object initialized
     
@@ -76,23 +76,23 @@ class SenseHatSensor():
         self.__temperature_02 = round(self.__sense.get_temperature_from_pressure(), SenseHatSensor.ROUNDING)
         self.__humidity = round(self.__sense.get_humidity(), SenseHatSensor.ROUNDING)
         self.__gyroscope_01 = round(
-            self.__sense.get_gyroscope_raw().get("x") * SenseHatSensor.GYROSCOPE_MULTIPLYER,
+            self.__sense.get_gyroscope_raw().get("x") * SenseHatSensor.GYROSCOPE_MULTIPLIER,
             SenseHatSensor.ROUNDING)
         self.__gyroscope_02 = round(
-            self.__sense.get_gyroscope_raw().get("y") * SenseHatSensor.GYROSCOPE_MULTIPLYER,
+            self.__sense.get_gyroscope_raw().get("y") * SenseHatSensor.GYROSCOPE_MULTIPLIER,
             SenseHatSensor.ROUNDING)
         self.__gyroscope_03 = round(
-            self.__sense.get_gyroscope_raw().get("z") * SenseHatSensor.GYROSCOPE_MULTIPLYER,
+            self.__sense.get_gyroscope_raw().get("z") * SenseHatSensor.GYROSCOPE_MULTIPLIER,
             SenseHatSensor.ROUNDING)
         self.__compass_north = round(self.__sense.get_compass(), SenseHatSensor.ROUNDING)
         self.__acceleration_01 = round(
-            self.__sense.get_accelerometer_raw().get("x") * SenseHatSensor.ACCELERATION_MULTIPLYER,
+            self.__sense.get_accelerometer_raw().get("x") * SenseHatSensor.ACCELERATION_MULTIPLIER,
             SenseHatSensor.ROUNDING)
         self.__acceleration_02 = round(
-            self.__sense.get_accelerometer_raw().get("y") * SenseHatSensor.ACCELERATION_MULTIPLYER,
+            self.__sense.get_accelerometer_raw().get("y") * SenseHatSensor.ACCELERATION_MULTIPLIER,
             SenseHatSensor.ROUNDING)
         self.__acceleration_03 = round(
-            self.__sense.get_accelerometer_raw().get("z") * SenseHatSensor.ACCELERATION_MULTIPLYER,
+            self.__sense.get_accelerometer_raw().get("z") * SenseHatSensor.ACCELERATION_MULTIPLIER,
             SenseHatSensor.ROUNDING)
         # generate and update data structure
         data = {
@@ -123,5 +123,6 @@ class SenseHatSensor():
                 }
             }
         }
+        # TODO: log sensor read call
         return data
 
--- utils/config.py
+++ utils/config.py
@@ -11,11 +11,11 @@ class Configuration():
     Class that generates a configuration object from an initialization (INI) file
     """
     
-    # class fallbacks for required variables
+    # fallbacks for configuration variables
     # DEFAULT
     RESOLUTION = 300
     # MQTT
-    CLIENT_ID = 'SenseHAT_RND'+str(randint(0,99))
+    MQTT_CLIENT_ID = 'sensehat_rnd'
     MQTT_BROKER_ADDRESS = 'mqtt://127.0.0.1:1883'
     MQTT_CHANNEL = "hass"
     MQTT_ZONE = "downstairs"
@@ -27,17 +27,44 @@ class Configuration():
     LOG_FORMAT = '%(asctime)s.%(msecs)03d %(levelname)s\t[%(name)s] %(message)s'
     LOG_DATEFMT = '%Y-%m-%dT%H:%M:%S'
     LOG_LEVEL = 'INFO'
+    # SENSEHAT
+    SENSEHAT_LOW_LIGHT = True
+    SENSEHAT_ROUNDING = 4
+    SENSEHAT_ACCELERATION_MULTIPLIER = 9.80665
+    SENSEHAT_GYROSCOPE_MULTIPLIER = 1.0
 
     def __init__(self, config_dir = './', config_file = 'CONFIG.ini'):
-        self.config_full_path_file = config_dir + config_file
-        if not path.isfile(self.config_full_path_file):
+        if not path.isfile(config_dir + config_file):
             # TODO: log error here
             raise OSError(filename=self.config_full_path_file)
+        self.config_full_path_file = config_dir + config_file
+        # init raw config with None and then set keys and values from INI file via load method
         self.__raw_config = None
-        self.__load_config()
-        # TODO: validate data here
+        self.__load_raw_config()
+        # init config attributes with class defaults and then set valus from raw config via load method
+        self.__resolution = Configuration.RESOLUTION
+        self.__welcome_msg = None
+        self.__mqtt_broker_address = Configuration.MQTT_BROKER_ADDRESS
+        self.__mqtt_client_id = Configuration.MQTT_CL+str(randint(0,99))
+        self.__mqtt_user = None
+        self.__mqtt_password = None
+        self.__mqtt_credentials_enabled = False
+        self.__mqtt_channel = Configuration.MQTT_CHANNEL
+        self.__mqtt_zone = Configuration.MQTT_ZONE
+        self.__mqtt_room = Configuration.MQTT_ROOM
+        self.__mqtt_sensor = Configuration.MQTT_SENSOR
+        self.__log_filename = Configuration.LOG_DIR+Configuration.LOG_FILE
+        self.__log_format = Configuration.LOG_FORMAT
+        self.__log_datefmt = Configuration.LOG_DATEFMT
+        self.__log_level = Configuration.LOG_LEVEL
+        self.__sensehat_low_light = Configuration.SENSEHAT_LOW_LIGHT
+        self.__sensehat_rounding = Configuration.SENSEHAT_ROUNDING
+        self.__sensehat_acceleration_multiplier = Configuration.SENSEHAT_ACCELERATION_MULTIPLIER
+        self.__sensehat_gyroscope_multiplier = Configuration.SENSEHAT_GYROSCOPE_MULTIPLIER
+        self.__load_config_attributes()
+        # TODO: log config object fully initialized
 
-    def __load_config(self):
+    def __load_raw_config(self):
         config_parser = ConfigParser()
         try:
             config_parser.read(self.configPath)
@@ -47,111 +74,149 @@ class Configuration():
             # TODO: raise a custom error for main app to catch passing err
             pass
 
-    # Getters for the configuration keys in the config file
-    # Each getter validades and assigns fallbacks when appropriate
-    @property
-    def resolution(self):
+    def __load_config_attributes(self):
+        # DEFAULT
+        # resolution
         if 'resolution' in self.__raw_config['DEFAULT']:
             resolution = self.__raw_config['DEFAULT'].getint('resolution', Configuration.RESOLUTION)
             if resolution > 0:
-                return resolution
-        return Configuration.RESOLUTION
-    
-    @property
-    def welcome_msg(self):
+                self.__resolution = resolution
+        # welcome_msg
         if 'welcome_msg' in self.__raw_config['DEFAULT']:
-            welcome_msg = self.__raw_config['DEFAULT'].getint('welcome_msg', None)
-            return welcome_msg
-        return None
-    
-    @property
-    def mqtt_client_id(self):
+            self.__welcome_msg = self.__raw_config['DEFAULT'].get('welcome_msg', None)
+        # MQTT
+        # mqtt_client_id
         if 'mqtt' in self.__raw_config.sections():
-            client_id = self.__raw_config['mqtt'].get('client_id', Configuration.CLIENT_ID)
-            return client_id
-        return Configuration.CLIENT_ID
-
-    @property
-    def mqtt_broker_address(self):
+            self.__mqtt_client_id = self.__raw_config['mqtt'].get('client_id', Configuration.MQTT_CLIENT_ID)
+        # mqtt_broker_address
         if 'mqtt' in self.__raw_config.sections():
-            broker_address = self.__raw_config['mqtt'].get('broker_address', Configuration.MQTT_BROKER_ADDRESS)
-            return broker_address
-        return Configuration.MQTT_BROKER_ADDRESS
-
-    @property
-    def mqtt_user(self):
+            self.__mqtt_broker_address = self.__raw_config['mqtt'].get('broker_address', Configuration.MQTT_BROKER_ADDRESS)
+        # mqtt_user
         if 'mqtt' in self.__raw_config.sections():
-            user = self.__raw_config['mqtt'].get('user', None)
-            return user
-        return None
-
-    @property
-    def mqtt_password(self):
+            self.__mqtt_user = self.__raw_config['mqtt'].get('user', None)
+        # mqtt_password
         if 'mqtt' in self.__raw_config.sections():
-            password = self.__raw_config['mqtt'].get('password', None)
-            return password
-        return None
-
-    @property
-    def mqtt_credentials_enabled(self):
+            self.__mqtt_password = self.__raw_config['mqtt'].get('password', None)
+        # mqtt_credentials_enabled
         # assume that user is None if MQTT does not use credentials
-        return True if self.mqtt_user else False
-    
-    @property
-    def mqtt_channel(self):
+        self.__mqtt_credentials_enabled = True if self.mqtt_user else False
+        # mqtt_channel
         if 'mqtt' in self.__raw_config.sections():
-            channel = self.__raw_config['mqtt'].get('channel', Configuration.MQTT_CHANNEL)
-            return channel
-        return Configuration.MQTT_CHANNEL
-
-    @property
-    def mqtt_zone(self):
+            self.__mqtt_channel = self.__raw_config['mqtt'].get('channel', Configuration.MQTT_CHANNEL)
+        # mqtt_zone
         if 'mqtt' in self.__raw_config.sections():
-            zone = self.__raw_config['mqtt'].get('zone', Configuration.MQTT_ZONE)
-            return zone
-        return Configuration.MQTT_ZONE
-
-    @property
-    def mqtt_room(self):
+            self.__mqtt_zone = self.__raw_config['mqtt'].get('zone', Configuration.MQTT_ZONE)
+        # mqtt_room
         if 'mqtt' in self.__raw_config.sections():
-            room = self.__raw_config['mqtt'].get('room', Configuration.MQTT_ROOM)
-            return room
-        return Configuration.MQTT_ROOM
-
-    @property
-    def mqtt_sensor(self):
+            self.__mqtt_room = self.__raw_config['mqtt'].get('room', Configuration.MQTT_ROOM)
+        # mqtt_sensor
         if 'mqtt' in self.__raw_config.sections():
-            sensor = self.__raw_config['mqtt'].get('sensor', Configuration.MQTT_SENSOR)
-            return sensor
-        return Configuration.MQTT_SENSOR
-
-    @property
-    def log_filename(self):
+            self.__mqtt_sensor = self.__raw_config['mqtt'].get('sensor', Configuration.MQTT_SENSOR)
+        # log_filename
         if 'log' in self.__raw_config.sections():
             log_filename = \
                 self.__raw_config['log'].get('dir', Configuration.LOG_DIR) \
                 + self.__raw_config['log'].get('dir', Configuration.LOG_FILE)
-            if path.isfilename(log_filename):
-                return log_filename
-        return Configuration.LOG_DIR
+            self.__log_filename = log_filename if path.isfilename(log_filename) else \
+                Configuration.LOG_DIR+Configuration.LOG_FILE
+        # log_format
+        if 'log' in self.__raw_config.sections():
+            self.__log_format = self.__raw_config['log'].get('format', Configuration.LOG_FORMAT)
+        # log_datefmt
+        if 'log' in self.__raw_config.sections():
+            self.__log_datefmt = self.__raw_config['log'].get('datefmt', Configuration.LOG_DATEFMT)
+        # log_level
+        if 'log' in self.__raw_config.sections():
+            self.__log_level = self.__raw_config['log'].get('level', Configuration.LOG_LEVEL)
+        # SENSEHAT
+        # sensehat_low_light
+        if 'sensehat' in self.__raw_config.sections():
+            self.__sensehat_low_light = self.__raw_config['sensehat'].getboolean('low_lebel', Configuration.LOG_LEVEL)
+        # sensehat_rounding
+        if 'sensehat' in self.__raw_config.sections():
+            self.__sensehat_rounding = self.__raw_config['sensehat'].getint('sensehat_rounding', Configuration.SENSEHAT_ROUNDING)
+        # sensehat_acceleration_multiplier
+        if 'sensehat' in self.__raw_config.sections():
+            self.__sensehat_acceleration_multiplier= self.__raw_config['sensehat'].getfloat('sensehat_acceleration_multiplier',
+                Configuration.SENSEHAT_ACCELERATION_MULTIPLIER)
+        # sensehat_gyroscope_multiplier
+        if 'sensehat' in self.__raw_config.sections():
+            self.__sensehat_gyroscope_multiplier = self.__raw_config['sensehat'].getfloat('sensehat_gyroscope_multiplier',
+                Configuration.SENSEHAT_GYROSCOPE_MULTIPLIER)
+
+    # TODO: Validation could happen in setter logic
+    @property
+    def resolution(self):
+        return self.__resolution
+    
+    @property
+    def welcome_msg(self):
+        return self.__welcome_msg
+    
+    @property
+    def mqtt_client_id(self):
+        return self.__mqtt_client_id
+
+    @property
+    def mqtt_broker_address(self):
+        return self.__mqtt_broker_address
+
+    @property
+    def mqtt_user(self):
+        return self.__mqtt_user
+
+    @property
+    def mqtt_password(self):
+        return self.__mqtt_password
+
+    @property
+    def mqtt_credentials_enabled(self):
+        return self.__mqtt_credentials_enabled
+    
+    @property
+    def mqtt_channel(self):
+        return self.__mqtt_channel
+
+    @property
+    def mqtt_zone(self):
+        return self.__mqtt_zone
+
+    @property
+    def mqtt_room(self):
+        return self.__mqtt_room
+
+    @property
+    def mqtt_sensor(self):
+        return self.__mqtt_sensor
+
+    @property
+    def log_filename(self):
+        return self.__log_filename
 
     @property
     def log_format(self):
-        if 'log' in self.__raw_config.sections():
-            log_format = self.__raw_config['log'].get('format', Configuration.LOG_FORMAT)
-            return log_format
-        return Configuration.LOG_FORMAT
+        return self.__log_format
 
     @property
     def log_datefmt(self):
-        if 'log' in self.__raw_config.sections():
-            log_datefmt = self.__raw_config['log'].get('datefmt', Configuration.LOG_DATEFMT)
-            return log_datefmt
-        return Configuration.LOG_DATEFMT
+        return self.__log_datefmt
 
     @property
     def log_level(self):
-        if 'log' in self.__raw_config.sections():
-            log_level = self.__raw_config['log'].get('level', Configuration.LOG_LEVEL)
-            return log_level
-        return Configuration.LOG_LEVEL
+        return self.__log_level
+    
+    @property
+    def sensehat_low_light(self):
+        return self.__sensehat_low_light
+    
+    @property
+    def sensehat_rounding(self):
+        return self.__sensehat_rounding
+
+    @property
+    def sensehat_acceleration_multiplier(self):
+        return self.__sensehat_acceleration_multiplier
+
+    @property
+    def sensehat_gyroscope_multiplier(self):
+        return self.__sensehat_gyroscope_multiplier
